b27b5e104f75ea1409be7cf1172e1a9312476e5d
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-15 15:10:33 -0800

    feat(routes): Add client-side routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
 import App from './components/App';
+import Subreddits from './components/Subreddits';
+import Authorize from './components/Authorize';
+
+// Route config
+const routes = (
+  <Route path="/" component={App}>
+    <Route path="/subreddits" component={Subreddits} />
+    <Route path="/authorize" component={Authorize} />
+  </Route>
+);
 
 render(
-  <App />,
+  <Router history={browserHistory} routes={routes} />,
   document.querySelector('.mount')
 );
