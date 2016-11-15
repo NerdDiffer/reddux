773494dd32f691bb19f97765aaf65329d0ca1107
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-15 15:52:41 -0800

    fix(api): Fix env vars
    With this plugin, the keys in template must match keys in the real thing!

--- src/api/subreddits.js
+++ src/api/subreddits.js
@@ -1,16 +1,9 @@
 // Get the user subreddits
+import { setAuthorizationHeader } from './config';
 
 export default client => {
   const url = '/subreddits/mine/subscriber';
-  const { REDDIT_CLIENT_ID, REDDIT_SECRET, REDDIT_ACCESS_TOKEN } = process.env;
-  console.log({ REDDIT_CLIENT_ID, REDDIT_SECRET, REDDIT_ACCESS_TOKEN });
-
-  const authValue = `Bearer ${REDDIT_ACCESS_TOKEN}`;
-  // TODO: remove this & refresh your access token if the process.env reference
-  // starts showing up!
-  const authValue_hardcoded = 'Bearer PuAtJ7CoKm4RsFiZ_6SWe04vJ7E';
-
-  const headers = { 'Authorization': authValue_hardcoded };
+  const headers = setAuthorizationHeader();
 
   return client.get(url, { headers })
     .then(res => {
