edebdb334dc6445518f13c0c8dd852f0644d116b
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-15 15:07:50 -0800

    feat(App): Add semantic-ui components to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container, Segment } from 'semantic-ui-react';
+import Nav from './Nav';
 
 const App = () => (
-  <div className="app">
-    <h1>Reddit Reader</h1>
-  </div>
+  <Container
+    className="app"
+    textAlign="center"
+    text
+  >
+    <Segment>
+      <Nav />
+      <h1>Reddit Reader</h1>
+    </Segment>
+  </Container>
 );
 
 export default App;
