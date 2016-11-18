3c4bfb4738148d75931bd4110e0ca3c023612d3c
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-18 15:04:23 -0800

    fix(api): Fix params & exports for accessToken
    Fun fact: the representation of data in the body of a POST request must
    match the 'content-type' header.

--- src/api/accessToken.js
+++ src/api/accessToken.js
@@ -1,19 +1,21 @@
+import { stringify } from 'querystring';
 import { generateBasicHeader, showError } from './_shared';
 
-const postForToken = (client, params) => {
+const postForToken = params => {
   const url = 'https://www.reddit.com/api/v1/access_token';
 
+  const body = stringify(params);
   const { REDDIT_CLIENT_ID: username, REDDIT_SECRET: password } = process.env;
 
   const config = {
     method: 'POST',
-    body: params,
+    body,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
       ...generateBasicHeader(username, password)
     },
     cache: 'no-cache'
-  },
+  };
 
   return fetch(url, config)
     .then(res => {
@@ -26,24 +28,24 @@ const postForToken = (client, params) => {
 };
 
 // https://github.com/reddit/reddit/wiki/OAuth2#retrieving-the-access-token
-export const retrieve = (client, code) => {
+export const retrieve = code => {
   const params = {
     grant_type: 'authorization_code',
     code,
     redirect_uri: 'http://localhost:8080/oauth/callback'
   };
 
-  return postForToken.call(null, client, params);
+  return postForToken.call(null, params);
 };
 
 // https://github.com/reddit/reddit/wiki/OAuth2#refreshing-the-token
-export const refresh = (client, refresh_token) => {
+export const refresh = refresh_token => {
   const params = {
     grant_type: 'refresh_token',
     refresh_token
   };
 
-  return postForToken.call(null, client, params);
+  return postForToken.call(null, params);
 };
 
 // TODO: add code for revoking token (ie: when user logs out)
--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,8 @@
-import Axios from 'axios';
 import { stringify } from 'querystring';
 
 import * as subreddits from './subreddits';
 import getFrontPage from './frontPage';
 import * as accessToken from './accessToken';
-
-const client = Axios.create({
-  baseURL: 'https://oauth.reddit.com'
-});
 
 // https://github.com/reddit/reddit/wiki/OAuth2#authorization
 const authUrl = (() => {
@@ -24,5 +19,4 @@ const authUrl = (() => {
   return `${baseUrl}?${stringifiedParams}`;
 })();
 
-export default client;
 export { authUrl, subreddits, accessToken, getFrontPage };
