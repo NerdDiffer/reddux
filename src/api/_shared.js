d99eeada7fdc5c7ee6699185e527f6d9f3248a45
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-18 13:22:50 -0800

    refactor(api): Clean up _shared module
    - Only need to look up one environment variable for header helper

--- src/api/_shared.js
+++ src/api/_shared.js
@@ -1,5 +1,5 @@
 export const setAuthorizationHeader = () => {
-  const { REDDIT_CLIENT_ID, REDDIT_SECRET, REDDIT_ACCESS_TOKEN } = process.env;
+  const { REDDIT_ACCESS_TOKEN } = process.env;
   const authValue = `Bearer ${REDDIT_ACCESS_TOKEN}`;
   return { 'Authorization': authValue };
 };
