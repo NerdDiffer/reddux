e8efa9e4bc0dff42717777e4977cb96001836420
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-20 22:29:48 -0800

    feat(Auth): Add loading icon to button
    It's based on app state

--- src/components/OAuth/RetrieveToken.jsx
+++ src/components/OAuth/RetrieveToken.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Button } from 'semantic-ui-react';
 
-const RetrieveToken = ({ handleClick }) => (
+const RetrieveToken = ({ handleClick, loading }) => (
   <Button
     content="Refresh token"
+    loading={loading}
     onClick={handleClick}
   />
 );
+
+RetrieveToken.propTypes = {
+  handleClick: PropTypes.func,
+  loading: PropTypes.bool
+};
 
 export default RetrieveToken;
