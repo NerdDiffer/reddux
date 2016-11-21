8ac5e070d0b8de6bb74e355b8bfc73356d20f225
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-20 17:58:53 -0800

    refactor(Auth): Convert Authorize to a class component
    Need to conditionally show buttons based on presence of tokens.

--- src/components/OAuth/Authorize.jsx
+++ src/components/OAuth/Authorize.jsx
@@ -3,22 +3,63 @@ import { Button } from 'semantic-ui-react';
 import { authUrl, accessToken } from '../../api';
 import { accessTokenStorage, refreshTokenStorage } from '../../utils/storage';
 
-const refresh = () => {
-  const token = refreshTokenStorage.get();
-  return accessToken.refresh(token)
-    .then(data => {
-      console.log(data);
-      accessTokenStorage.set(data.access_token);
-      refreshTokenStorage.clear();
-    });
-};
+class Authorize extends Component {
+  constructor(props) {
+    super(props);
 
-const Authorize = () => (
-  <div className="auth link">
-    <a href={authUrl}>Authorize Reddit</a>
-    <br />
-    <Button content="Refresh token" onClick={refresh} />
-  </div>
-);
+    this.state = {
+      hasAccessToken: !!accessTokenStorage.get(),
+      hasRefreshToken: !!refreshTokenStorage.get()
+    };
+
+    this.handleRequestRefreshToken = this.handleRequestRefreshToken.bind(this);
+  }
+
+  handleRequestRefreshToken() {
+    const token = refreshTokenStorage.get();
+
+    return accessToken.refresh(token)
+      .then(data => {
+        console.log(data);
+        accessTokenStorage.set(data.access_token);
+        refreshTokenStorage.clear();
+      })
+      .then(() => {
+        this.setState({
+          hasAccessToken: true,
+          hasRefreshToken: false
+        });
+      })
+  }
+
+  handleRevokeTokens() {
+    // TODO: add handler & UI for manually revoking tokens
+  }
+
+  renderLink() {
+    const { hasAccessToken, hasRefreshToken } = this.state;
+
+    if (!hasAccessToken || !hasRefreshToken) {
+      return (<a href={authUrl}>Authorize Reddit</a>);
+    } else {
+      return (
+        <Button
+          content="Refresh token"
+          onClick={this.handleRequestRefreshToken}
+        />
+      );
+    }
+  }
+
+  render() {
+    const { hasAccessToken, hasRefreshToken } = this.state;
+
+    return (
+      <div className="auth link">
+        {this.renderLink()}
+      </div>
+    );
+  }
+};
 
 export default Authorize;
