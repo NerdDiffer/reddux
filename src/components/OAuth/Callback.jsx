2ad45d852e8d90f4e508e2959cb3fb26836660bc
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-20 22:07:59 -0800

    fix(Auth): Cleanup Callback component
    - Update references to methods
    - Remove state (only need to use dispatchers)
    - Make render method more succinct

--- src/components/OAuth/Callback.jsx
+++ src/components/OAuth/Callback.jsx
@@ -1,32 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { handleAuthorization } from '../../state/actions/auth';
+import { handleAuthCallback } from '../../state/actions/auth';
 
 class OAuthCallback extends Component {
   componentDidMount() {
     const { error, code } = this.props.location.query;
-    return this.props.handleAuthorization({ error, code });
+    return this.props.handleAuthCallback({ error, code });
   }
 
   render() {
-    return(
-      <div className="clientTest">
-      </div>
-    );
+    return (<div className="auth callback" />);
   }
 }
 
-const mapStateToProps = ({ auth }) => ({
-  auth
-});
-
 const mapDispatchToProps = dispatch => (
-  bindActionCreators({ handleAuthorization }, dispatch)
+  bindActionCreators({ handleAuthCallback }, dispatch)
 );
 
 const ConnectedCallback = connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(OAuthCallback);
 
